9730e6358cd07ab976f3b77d822487d90380a7f1
Marat <Shagiakhmetov2499@gmail.com>
2022-10-11 10:57:51 +0300

    Initial Commit

---
+++ Nasa_repetition_2.10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nasa_repetition_2.10
+//
+//  Created by Marat Shagiakhmetov on 11.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
